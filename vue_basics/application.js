7245fe7ec6c30a8071f41ca5a969c39e5b18d419
Abhishek <lovestory1990@gmail.com>
2022-09-19 22:54:03 +0530

    vue app updated

--- vue_basics/application.js
+++ vue_basics/application.js
@@ -1,2 +1,26 @@
 let message="Hello world";
-document.getElementById("app").innerHTML=message;
+
+var app=new Vue({
+    el:"#app",
+    data:{
+        message:"Hello World",
+        
+        visitor_name:"",
+        visitors:[]
+    },
+
+    methods: {
+        sayHi: function(){
+            this.message="Hi";
+            this.visitors.push(this.visitor_name);
+            this.visitor_name="";
+        }
+
+        
+    },
+    computed :{
+        count: function(){
+            return this.visitors.length;
+        }
+    }
+})
