c6d3b1918261a8deb4d6290bbe0fd05ba8b8752e
Abhishek <lovestory1990@gmail.com>
2022-10-04 17:16:55 +0530

    lecture-5

--- vue_test/application.js
+++ vue_test/application.js
@@ -1,21 +1,44 @@
-
-
+// const comp1 ={
+//     template:`<h4>Hello: {{message}}</h4>`,
+//     data: {
         
-        const c = new Vue({
-            el:'#app3',
-            data: {
-            message: 'Abhishek',
-            },
-            })
-            const a = new Vue({
-                el:'#app1',
-                data: {
-                message: "Kumar",
-                },
-                })
-                const b = new Vue({
-                    el:'#app2',
-                    data: {
-                    message: "Rajput",
-                    },
-                    })
+//             message: 'Welcome to IITM'
+        
+//     },
+// }
+
+const players = [
+    {name:'Rohit Sharma', role:'Batsman', team:'MI'},
+    {name:'Jonathan Burger', role:'Batsman', team:'RCB'},
+    {name:'Jaspreet Bumrah', role:'Batsman', team:'MI'},
+]
+const app = new Vue({
+    el:'#app',
+    data:{
+        players: players,
+    },
+    // data:{
+    //     x:20,
+    //     y:40
+    // },
+    // methods:{
+    //     addItem:function(){
+    //         items.push('New Item');
+    //     }
+    // },
+    // computed:{
+    //     simpleInterest(){
+    //         return(this.principal* this.annualInterestRate* this.duration)/100
+    //     },
+    // },
+    // created(){
+    //     console.log(this.x)
+    // },
+})
+// let handler=setInterval(() =>{
+//     data=appData.pop();
+//     app.principal=data[0];
+//     app.annualInterestRate=data[1];
+//     app.duration=data[2];
+//     app.totalPayableAmount +=app.simpleInterest
+// },1000)
