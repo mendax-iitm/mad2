2383b74d5b1bfe38b9b7296a5f9be3674aa97572
Abhishek <lovestory1990@gmail.com>
2022-10-26 23:28:11 +0530

    test on vestura

--- vue_test/application.js
+++ vue_test/application.js
@@ -1,44 +1,65 @@
-// const comp1 ={
-//     template:`<h4>Hello: {{message}}</h4>`,
-//     data: {
-        
-//             message: 'Welcome to IITM'
-        
-//     },
-// }
-
-const players = [
-    {name:'Rohit Sharma', role:'Batsman', team:'MI'},
-    {name:'Jonathan Burger', role:'Batsman', team:'RCB'},
-    {name:'Jaspreet Bumrah', role:'Batsman', team:'MI'},
-]
-const app = new Vue({
-    el:'#app',
-    data:{
-        players: players,
+const namedSlot={
+    template:`
+    <div>
+    <slot name ="header" :user="currentUser"></slot>
+    </div>`,
+    data(){
+    return {
+    users: [{
+    user_id:1,
+    name:'Narendra',
+    dist:'Ballia',
     },
-    // data:{
-    //     x:20,
-    //     y:40
-    // },
-    // methods:{
-    //     addItem:function(){
-    //         items.push('New Item');
-    //     }
-    // },
-    // computed:{
-    //     simpleInterest(){
-    //         return(this.principal* this.annualInterestRate* this.duration)/100
-    //     },
-    // },
-    // created(){
-    //     console.log(this.x)
-    // },
-})
-// let handler=setInterval(() =>{
-//     data=appData.pop();
-//     app.principal=data[0];
-//     app.annualInterestRate=data[1];
-//     app.duration=data[2];
-//     app.totalPayableAmount +=app.simpleInterest
-// },1000)
+    {
+    user_id:2,
+    name:'Abhishek',
+    dist:'Delhi',
+    },
+    ],
+    currentUser:null,
+    }
+    },
+    props:['current_user_id'],
+    created(){
+    current_user=sessionStorage.getItem('userId')
+    current_user_id=current_user ? current_user:2
+    this.currentUser = this.users.find(
+    (user) => user.user_id == current_user_id
+    )
+    sessionStorage.setItem('userId',current_user=1?2:1)
+    },
+    }
+    
+    const app=new Vue({
+    el:'#cards',
+    data:{
+        players:['player2','player1','player3','player4'],
+        cards:{
+            player1:null,
+            player2:null,
+            player3:null,
+            player4:null,
+        },
+        currentPlayer:null,
+        previousPlayer:null,
+        error:null,
+    },
+    methods:{
+        validate(){
+            card=event.target.value.split('-')[0]
+            this.cards[this.currentPlayer]=card
+            this.error=null
+            if(this.cards[this.currentPlayer] == this.cards[this.previousPlayer]){
+                this.error='You cannot pick this cards'
+                this.cards[this.currentPlayer]=null
+            }else{
+                this.previousPlayer=this.currentPlayer
+                this.currentPlayer=this.players.pop()
+            }
+        },
+    },
+    created(){
+        this.currentPlayer=this.players.pop()
+        this.previousPlayer=this.players[0]
+    },
+    })
